3c066782720ce64bf1c3878542fc905c4a6f319c
randomittin <rj@superpe.in>
2024-04-24 18:52:34 +0530

    first commit

---
+++ src/data.tsx
@@ -0,0 +1,300 @@
+export const mails = [
+  {
+    id: 'first-onboarding',
+    name: '#1 : Get started',
+    email: 'onboarding@superpe.in',
+    subject: 'First step to get your onboarding started',
+    text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next steps to ensure the project's success.\n\nPlease come prepared with any questions or insights you may have. Looking forward to our meeting!\n\nBest regards, William",
+    date: '2024-04-20T09:00:00',
+    read: false,
+    labels: ['#1', 'onboarding', 'important']
+  },
+  {
+    id: '6c84fb90-12c4-11e1-840d-7b25ce775a',
+    name: '#2 : Integrate with store',
+    email: 'onboarding@superpe.in',
+    subject: 'Integrate with your shopify right away',
+    text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next steps to ensure the project's success.\n\nPlease come prepared with any questions or insights you may have. Looking forward to our meeting!\n\nBest regards, William",
+    date: '2024-04-20T09:00:00',
+    read: false,
+    labels: ['#2', 'onboarding', 'important']
+  },
+  {
+    id: '6c84fb90-12c4-11e1-840d-7c5ee775a',
+    name: '#3 : All set for payments',
+    email: 'onboarding@superpe.in',
+    subject: "That's it! Let's accept your first payment",
+    text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next steps to ensure the project's success.\n\nPlease come prepared with any questions or insights you may have. Looking forward to our meeting!\n\nBest regards, William",
+    date: '2024-04-20T09:00:00',
+    read: false,
+    labels: ['#3', 'onboarding', 'important']
+  },
+  {
+    id: '3e7c3f6d-bdf5-46ae-8d90-171300f27ae2',
+    name: 'Rishabh Jain',
+    email: 'rj@superpe.in',
+    subject: "I'm glad to see you here",
+    text: "Hey there! \n\n I'm Rishabh, founder of SuperPe. \n We are super excited to have you onboard & welcome you to the SuperPe family. We'll do everything possible to make sure you have a great experience with us.\n\nThanks,\nRJ\nFounder ✦ SuperPe",
+    date: '2023-04-10T11:45:00',
+    read: true,
+    labels: ['personal']
+  },
+  {
+    id: '61c35085-72d7-42b4-8d62-738f700d4b92',
+    name: 'Emily Davis',
+    email: 'emilydavis@example.com',
+    subject: 'Re: Question about Budget',
+    text: "I have a question about the budget for the upcoming project. It seems like there's a discrepancy in the allocation of resources.\n\nI've reviewed the budget report and identified a few areas where we might be able to optimize our spending without compromising the project's quality.\n\nI've attached a detailed analysis for your reference. Let's discuss this further in our next meeting.\n\nThanks, Emily",
+    date: '2023-03-25T13:15:00',
+    read: false,
+    labels: ['work', 'budget']
+  },
+  {
+    id: '8f7b5db9-d935-4e42-8e05-1f1d0a3dfb97',
+    name: 'Michael Wilson',
+    email: 'michaelwilson@example.com',
+    subject: 'Important Announcement',
+    text: "I have an important announcement to make during our team meeting. It pertains to a strategic shift in our approach to the upcoming product launch. We've received valuable feedback from our beta testers, and I believe it's time to make some adjustments to better meet our customers' needs.\n\nThis change is crucial to our success, and I look forward to discussing it with the team. Please be prepared to share your insights during the meeting.\n\nRegards, Michael",
+    date: '2023-03-10T15:00:00',
+    read: false,
+    labels: ['meeting', 'work', 'important']
+  },
+  {
+    id: '1f0f2c02-e299-40de-9b1d-86ef9e42126b',
+    name: 'Sarah Brown',
+    email: 'sarahbrown@example.com',
+    subject: 'Re: Feedback on Proposal',
+    text: "Thank you for your feedback on the proposal. It looks great! I'm pleased to hear that you found it promising. The team worked diligently to address all the key points you raised, and I believe we now have a strong foundation for the project.\n\nI've attached the revised proposal for your review.\n\nPlease let me know if you have any further comments or suggestions. Looking forward to your response.\n\nBest regards, Sarah",
+    date: '2023-02-15T16:30:00',
+    read: true,
+    labels: ['work']
+  },
+  {
+    id: '17c0a96d-4415-42b1-8b4f-764efab57f66',
+    name: 'David Lee',
+    email: 'davidlee@example.com',
+    subject: 'New Project Idea',
+    text: "I have an exciting new project idea to discuss with you. It involves expanding our services to target a niche market that has shown considerable growth in recent months.\n\nI've prepared a detailed proposal outlining the potential benefits and the strategy for execution.\n\nThis project has the potential to significantly impact our business positively. Let's set up a meeting to dive into the details and determine if it aligns with our current goals.\n\nBest regards, David",
+    date: '2023-01-28T17:45:00',
+    read: false,
+    labels: ['meeting', 'work', 'important']
+  },
+  {
+    id: '2f0130cb-39fc-44c4-bb3c-0a4337edaaab',
+    name: 'Olivia Wilson',
+    email: 'oliviawilson@example.com',
+    subject: 'Vacation Plans',
+    text: "Let's plan our vacation for next month. What do you think? I've been thinking of visiting a tropical paradise, and I've put together some destination options.\n\nI believe it's time for us to unwind and recharge. Please take a look at the options and let me know your preferences.\n\nWe can start making arrangements to ensure a smooth and enjoyable trip.\n\nExcited to hear your thoughts! Olivia",
+    date: '2022-12-20T18:30:00',
+    read: true,
+    labels: ['personal']
+  },
+  {
+    id: 'de305d54-75b4-431b-adb2-eb6b9e546014',
+    name: 'James Martin',
+    email: 'jamesmartin@example.com',
+    subject: 'Re: Conference Registration',
+    text: "I've completed the registration for the conference next month. The event promises to be a great networking opportunity, and I'm looking forward to attending the various sessions and connecting with industry experts.\n\nI've also attached the conference schedule for your reference.\n\nIf there are any specific topics or sessions you'd like me to explore, please let me know. It's an exciting event, and I'll make the most of it.\n\nBest regards, James",
+    date: '2022-11-30T19:15:00',
+    read: true,
+    labels: ['work', 'conference']
+  },
+  {
+    id: '7dd90c63-00f6-40f3-bd87-5060a24e8ee7',
+    name: 'Sophia White',
+    email: 'sophiawhite@example.com',
+    subject: 'Team Dinner',
+    text: "Let's have a team dinner next week to celebrate our success. We've achieved some significant milestones, and it's time to acknowledge our hard work and dedication.\n\nI've made reservations at a lovely restaurant, and I'm sure it'll be an enjoyable evening.\n\nPlease confirm your availability and any dietary preferences. Looking forward to a fun and memorable dinner with the team!\n\nBest, Sophia",
+    date: '2022-11-05T20:30:00',
+    read: false,
+    labels: ['meeting', 'work']
+  },
+  {
+    id: '99a88f78-3eb4-4d87-87b7-7b15a49a0a05',
+    name: 'Daniel Johnson',
+    email: 'danieljohnson@example.com',
+    subject: 'Feedback Request',
+    text: "I'd like your feedback on the latest project deliverables. We've made significant progress, and I value your input to ensure we're on the right track.\n\nI've attached the deliverables for your review, and I'm particularly interested in any areas where you think we can further enhance the quality or efficiency.\n\nYour feedback is invaluable, and I appreciate your time and expertise. Let's work together to make this project a success.\n\nRegards, Daniel",
+    date: '2022-10-22T09:30:00',
+    read: false,
+    labels: ['work']
+  },
+  {
+    id: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+    name: 'Ava Taylor',
+    email: 'avataylor@example.com',
+    subject: 'Re: Meeting Agenda',
+    text: "Here's the agenda for our meeting next week. I've included all the topics we need to cover, as well as time allocations for each.\n\nIf you have any additional items to discuss or any specific points to address, please let me know, and we can integrate them into the agenda.\n\nIt's essential that our meeting is productive and addresses all relevant matters.\n\nLooking forward to our meeting! Ava",
+    date: '2022-10-10T10:45:00',
+    read: true,
+    labels: ['meeting', 'work']
+  },
+  {
+    id: 'c1a0ecb4-2540-49c5-86f8-21e5ce79e4e6',
+    name: 'William Anderson',
+    email: 'williamanderson@example.com',
+    subject: 'Product Launch Update',
+    text: "The product launch is on track. I'll provide an update during our call. We've made substantial progress in the development and marketing of our new product.\n\nI'm excited to share the latest updates with you during our upcoming call. It's crucial that we coordinate our efforts to ensure a successful launch. Please come prepared with any questions or insights you may have.\n\nLet's make this product launch a resounding success!\n\nBest regards, William",
+    date: '2022-09-20T12:00:00',
+    read: false,
+    labels: ['meeting', 'work', 'important']
+  },
+  {
+    id: 'ba54eefd-4097-4949-99f2-2a9ae4d1a836',
+    name: 'Mia Harris',
+    email: 'miaharris@example.com',
+    subject: 'Re: Travel Itinerary',
+    text: "I've received the travel itinerary. It looks great! Thank you for your prompt assistance in arranging the details. I've reviewed the schedule and the accommodations, and everything seems to be in order. I'm looking forward to the trip, and I'm confident it'll be a smooth and enjoyable experience.\n\nIf there are any specific activities or attractions you recommend at our destination, please feel free to share your suggestions.\n\nExcited for the trip! Mia",
+    date: '2022-09-10T13:15:00',
+    read: true,
+    labels: ['personal', 'travel']
+  },
+  {
+    id: 'df09b6ed-28bd-4e0c-85a9-9320ec5179aa',
+    name: 'Ethan Clark',
+    email: 'ethanclark@example.com',
+    subject: 'Team Building Event',
+    text: "Let's plan a team-building event for our department. Team cohesion and morale are vital to our success, and I believe a well-organized team-building event can be incredibly beneficial. I've done some research and have a few ideas for fun and engaging activities.\n\nPlease let me know your thoughts and availability. We want this event to be both enjoyable and productive.\n\nTogether, we'll strengthen our team and boost our performance.\n\nRegards, Ethan",
+    date: '2022-08-25T15:30:00',
+    read: false,
+    labels: ['meeting', 'work']
+  },
+  {
+    id: 'd67c1842-7f8b-4b4b-9be1-1b3b1ab4611d',
+    name: 'Chloe Hall',
+    email: 'chloehall@example.com',
+    subject: 'Re: Budget Approval',
+    text: "The budget has been approved. We can proceed with the project. I'm delighted to inform you that our budget proposal has received the green light from the finance department. This is a significant milestone, and it means we can move forward with the project as planned.\n\nI've attached the finalized budget for your reference. Let's ensure that we stay on track and deliver the project on time and within budget.\n\nIt's an exciting time for us! Chloe",
+    date: '2022-08-10T16:45:00',
+    read: true,
+    labels: ['work', 'budget']
+  },
+  {
+    id: '6c9a7f94-8329-4d70-95d3-51f68c186ae1',
+    name: 'Samuel Turner',
+    email: 'samuelturner@example.com',
+    subject: 'Weekend Hike',
+    text: "Who's up for a weekend hike in the mountains? I've been craving some outdoor adventure, and a hike in the mountains sounds like the perfect escape. If you're up for the challenge, we can explore some scenic trails and enjoy the beauty of nature.\n\nI've done some research and have a few routes in mind.\n\nLet me know if you're interested, and we can plan the details.\n\nIt's sure to be a memorable experience! Samuel",
+    date: '2022-07-28T17:30:00',
+    read: false,
+    labels: ['personal']
+  }
+];
+
+export type Mail = (typeof mails)[number];
+
+export const accounts = [
+  {
+    label: 'Shopify',
+    email: 'Shopify Docs',
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' fill='#fff'>
+        <path d='M388.3 104.1a4.7 4.7 0 0 0 -4.4-4c-2 0-37.2-.8-37.2-.8s-21.6-20.8-29.6-28.8V503.2L442.8 472S388.7 106.5 388.3 104.1zM288.7 70.5a116.7 116.7 0 0 0 -7.2-17.6C271 32.9 255.4 22 237 22a15 15 0 0 0 -4 .4c-.4-.8-1.2-1.2-1.6-2C223.4 11.6 213 7.6 200.6 8c-24 .8-48 18-67.3 48.8-13.6 21.6-24 48.8-26.8 70.1-27.6 8.4-46.8 14.4-47.2 14.8-14 4.4-14.4 4.8-16 18-1.2 10-38 291.8-38 291.8L307.9 504V65.7a41.7 41.7 0 0 0 -4.4 .4S297.9 67.7 288.7 70.5zM233.4 87.7c-16 4.8-33.6 10.4-50.8 15.6 4.8-18.8 14.4-37.6 25.6-50 4.4-4.4 10.4-9.6 17.2-12.8C232.2 54.9 233.8 74.5 233.4 87.7zM200.6 24.4A27.5 27.5 0 0 1 215 28c-6.4 3.2-12.8 8.4-18.8 14.4-15.2 16.4-26.8 42-31.6 66.5-14.4 4.4-28.8 8.8-42 12.8C131.3 83.3 163.8 25.2 200.6 24.4zM154.2 244.6c1.6 25.6 69.3 31.2 73.3 91.7 2.8 47.6-25.2 80.1-65.7 82.5-48.8 3.2-75.7-25.6-75.7-25.6l10.4-44s26.8 20.4 48.4 18.8c14-.8 19.2-12.4 18.8-20.4-2-33.6-57.2-31.6-60.8-86.9-3.2-46.4 27.2-93.3 94.5-97.7 26-1.6 39.2 4.8 39.2 4.8L221.4 225.4s-17.2-8-37.6-6.4C154.2 221 153.8 239.8 154.2 244.6zM249.4 82.9c0-12-1.6-29.2-7.2-43.6 18.4 3.6 27.2 24 31.2 36.4Q262.6 78.7 249.4 82.9z' />
+      </svg>
+    )
+  },
+  {
+    label: 'Website',
+    email: 'Web Docs',
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' fill='#fff'>
+        <path
+          class='fa-secondary'
+          opacity='.4'
+          d='M96 96a32 32 0 1 0 0 64 32 32 0 1 0 0-64zM448 480c35.3 0 64-28.7 64-64V224L0 224V416c0 35.3 28.7 64 64 64l384 0z'
+        />
+        <path
+          class='fa-primary'
+          d='M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V224H0V96zm64 32a32 32 0 1 0 64 0 32 32 0 1 0 -64 0zm120-24c-13.3 0-24 10.7-24 24s10.7 24 24 24H424c13.3 0 24-10.7 24-24s-10.7-24-24-24H184z'
+        />
+      </svg>
+    )
+  }
+];
+
+export type Account = (typeof accounts)[number];
+
+export const contacts = [
+  {
+    name: 'Emma Johnson',
+    email: 'emma.johnson@example.com'
+  },
+  {
+    name: 'Liam Wilson',
+    email: 'liam.wilson@example.com'
+  },
+  {
+    name: 'Olivia Davis',
+    email: 'olivia.davis@example.com'
+  },
+  {
+    name: 'Noah Martinez',
+    email: 'noah.martinez@example.com'
+  },
+  {
+    name: 'Ava Taylor',
+    email: 'ava.taylor@example.com'
+  },
+  {
+    name: 'Lucas Brown',
+    email: 'lucas.brown@example.com'
+  },
+  {
+    name: 'Sophia Smith',
+    email: 'sophia.smith@example.com'
+  },
+  {
+    name: 'Ethan Wilson',
+    email: 'ethan.wilson@example.com'
+  },
+  {
+    name: 'Isabella Jackson',
+    email: 'isabella.jackson@example.com'
+  },
+  {
+    name: 'Mia Clark',
+    email: 'mia.clark@example.com'
+  },
+  {
+    name: 'Mason Lee',
+    email: 'mason.lee@example.com'
+  },
+  {
+    name: 'Layla Harris',
+    email: 'layla.harris@example.com'
+  },
+  {
+    name: 'William Anderson',
+    email: 'william.anderson@example.com'
+  },
+  {
+    name: 'Ella White',
+    email: 'ella.white@example.com'
+  },
+  {
+    name: 'James Thomas',
+    email: 'james.thomas@example.com'
+  },
+  {
+    name: 'Harper Lewis',
+    email: 'harper.lewis@example.com'
+  },
+  {
+    name: 'Benjamin Moore',
+    email: 'benjamin.moore@example.com'
+  },
+  {
+    name: 'Aria Hall',
+    email: 'aria.hall@example.com'
+  },
+  {
+    name: 'Henry Turner',
+    email: 'henry.turner@example.com'
+  },
+  {
+    name: 'Scarlett Adams',
+    email: 'scarlett.adams@example.com'
+  }
+];
+
+export type Contact = (typeof contacts)[number];
